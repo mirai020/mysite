56094f09af95dab8bf81dd0ac6c735b169c89649
mirai020 <mickey._.1212@icloud.com>
2026-08-28 17:39:00 +0900

    Delete サイト制作ヤモリ directory

--- サイト制作ヤモリ/gecko.js
+++
@@ -1,8 +0,0 @@
-'ues strict'
-
-const btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-btn.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
